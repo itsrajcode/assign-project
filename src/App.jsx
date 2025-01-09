9990ec63d803cae6e11ad202568e149e81804eb6
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-01-09 15:21:26 +0530

    Fix: style fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Header from './components/Header'
-import ResultCard from './components/ResultCard'
+import React from "react";
+import Header from "./components/Header";
+import ResultCard from "./components/ResultCard";
+import CompareAccuracy from "./components/CompareAccuracy";
 
 function App() {
   return (
     <div>
       <Header />
-      <div className='p-5'>
+      <div className="p-5 flex">
         <ResultCard />
+        <div className="flex flex-row gap-4">
+          <CompareAccuracy />
+          <CompareAccuracy />
+          <CompareAccuracy />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CompareAccuracy.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BarChart2 } from 'lucide-react';
+
+const CompareAccuracy = () => {
+  return (
+    <div
+      className="p-4 rounded-lg bg-white shadow-sm border border-gray-200"
+      style={{ height: '200px', width: '300px' }}
+    >
+      <div className="flex items-center gap-2 text-gray-500">
+        <BarChart2 className="w-5 h-5" />
+        <span className="text-sm">Compare Accuracy</span>
+      </div>
+    </div>
+  );
+};
+
+export default CompareAccuracy;
